741df269bc4158d68fc854b944e50d3b052480e3
Chandrabhan-max <getabhi299@gmail.com>
2026-04-06 23:58:36 +0530

    added ssl for neon db

--- db.js
+++ db.js
@@ -1,20 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
   user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-});
-
-pool.on('connect', () => {
-  console.log('Connected to PostgreSQL');
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res) => {
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
